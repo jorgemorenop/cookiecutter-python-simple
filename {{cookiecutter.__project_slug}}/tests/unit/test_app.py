4bcfcb7323eddd725ab377741614423ca5530adb
jorge <jorgemorenodev@gmail.com>
2023-07-10 10:18:15 +0200

    Use package_name in some necessary places

--- {{cookiecutter.__project_slug}}/tests/unit/test_app.py
+++ {{cookiecutter.__project_slug}}/tests/unit/test_app.py
@@ -3,7 +3,7 @@
 
 # TODO: Replace with your own tests
 def test_dummy():
-    from example_project.app import dummy_function
+    from {{cookiecutter.package_name}}.app import dummy_function
 
     assert dummy_function(2, 2) == 4
     assert dummy_function(3, 5) == 8
